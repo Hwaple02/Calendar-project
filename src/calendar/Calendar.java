4141fed3c0ed297176630238bd47839d68f56800
Ben Kang <Ben Kang@DESKTOP-6RPFV0J>
2022-03-14 15:31:21 +0900

    메서드를 사용한 특정 달의 최대 일수 출력하기

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -3,15 +3,19 @@ package calendar;
 import java.util.Scanner;
 
 public class Calendar {
+	private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+	public int maxDay(int month)
+	{
+		return MAX_DAYS[month-1];
+	}
 	public static void main(String[] args) {
 //입력받은 월의 최대 일수 출력하기
-		int[] month = {31,28,31,30,31,30,31,31,30,31,30,31};
 		System.out.println("달을 입력하세요.");
 		Scanner scanner = new Scanner(System.in);
-		String s = scanner.next();
-		int n = Integer.parseInt(s);
-		
-		System.out.printf("%d월은 %d일까지 있습니다.", n, month[n-1]);
-
+		Calendar cal = new Calendar();
+		int n = scanner.nextInt();		
+		System.out.printf("%d월은 %d일까지 있습니다.", n, cal.maxDay(n));
+		scanner.close();
 	}
 }
