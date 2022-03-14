045dcf107abd2236edd893439a821de9ef97df48
Ben Kang <Ben Kang@DESKTOP-6RPFV0J>
2022-03-14 14:43:37 +0900

    두 수를 입력받아 출력하기

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -1,12 +1,21 @@
 package calendar;
 
+import java.util.Scanner;
+
 public class Calendar {
 	public static void main(String[] args) {
-		System.out.println("  일 월  화 수  목  금 토");
-		System.out.println(" ---------------------");
-		System.out.println("  1  2  3  4  5  6  7");
-		System.out.println("  8  9 10 11 12 13 14");
-		System.out.println(" 15 16 17 18 19 20 21");
-		System.out.println(" 22 23 24 25 26 27 28");
+//입력받은 두 수의 합을 출력하기
+		System.out.println("두 수를 입력하세요.");
+		
+		Scanner s = new Scanner(System.in);
+		String a = s.next();
+		String b = s.next();
+		
+		int i = Integer.parseInt(a);
+		int k = Integer.parseInt(b);
+		
+		//System.out.println("두 수의 합은 " + (i+k) + "입니다.");
+		System.out.printf("%d와 %d의 합은 %d입니다.", i, k, (i+k));
+		s.close();
 	}
 }
